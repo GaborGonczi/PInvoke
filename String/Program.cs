cf6b0fe60f4aa97f57b241ce13a47570658df631
Gabor Gonczi <gonczigabor98@gmail.com>
2020-12-23 20:30:44 +0100

    Some dllimport added

--- String/Program.cs
+++ String/Program.cs
@@ -8,6 +8,16 @@ namespace ConsoleApp2
 {
     class Program
     {
+
+        [DllImport("D:\\SzE\\Programozassajat\\szerepjatek\\c++&c#\\CPPDLL\\x64\\Debug\\CPPDLL.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern IntPtr create();
+        [DllImport("D:\\SzE\\Programozassajat\\szerepjatek\\c++&c#\\CPPDLL\\x64\\Debug\\CPPDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void set(string text, IntPtr myobj);
+        [DllImport("D:\\SzE\\Programozassajat\\szerepjatek\\c++&c#\\CPPDLL\\x64\\Debug\\CPPDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern string get(IntPtr myobj);
+        [DllImport("D:\\SzE\\Programozassajat\\szerepjatek\\c++&c#\\CPPDLL\\x64\\Debug\\CPPDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void deleteobj(IntPtr myobj);
+	
         static void Main(string[] args)
         {
         }
