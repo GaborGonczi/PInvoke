7f30ca8e96ffdcf5dbf42f5d23387e5d60e5d35a
GaborGonczi <55331670+GaborGonczi@users.noreply.github.com>
2020-12-23 20:23:10 +0100

    Update Program.cs

--- String/Program.cs
+++ String/Program.cs
@@ -8,6 +8,14 @@ namespace ConsoleApp2
 {
     class Program
     {
+        [DllImport("D:\\SzE\\Programozassajat\\szerepjatek\\c++&c#\\CPPDLL\\x64\\Debug\\CPPDLL.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern int addition(int num1, int num2);
+        [DllImport("D:\\SzE\\Programozassajat\\szerepjatek\\c++&c#\\CPPDLL\\x64\\Debug\\CPPDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int substraction(int num1, int num2);
+        [DllImport("D:\\SzE\\Programozassajat\\szerepjatek\\c++&c#\\CPPDLL\\x64\\Debug\\CPPDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int multiplication(int num1, int num2);
+        [DllImport("D:\\SzE\\Programozassajat\\szerepjatek\\c++&c#\\CPPDLL\\x64\\Debug\\CPPDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern double division(int num1, int num2);
         static void Main(string[] args)
         {
         }
